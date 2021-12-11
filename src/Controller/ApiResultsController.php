1f4a51cadeea79d94096bbcf54685a5e85ae93f2
karimbasly <karimbasli@live.fr>
2021-12-11 20:24:52 +0100

    added ResultApi

---
+++ src/Controller/ApiResultsController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use App\Utility\Utils;
+use Doctrine\ORM\EntityManagerInterface;
+use MiW\Results\Entity\Result;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+class ApiResultsController extends AbstractController
+{
+    /*
+}
+    public const RUTA_API = '/api/v1/results';
+
+    private const HEADER_CACHE_CONTROL = 'Cache-Control';
+    private const HEADER_ETAG = 'ETag';
+    private const HEADER_ALLOW = 'Allow';
+    private const ROLE_ADMIN = 'ROLE_ADMIN';
+
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->entityManager = $em;
+    }
+    /**
+     * CGET Action
+     * Summary: Retrieves the collection of Result.
+     * Notes: Returns all Result .
+     *
+     * @param   Request $request
+     * @return  Response
+     * @Route(
+     *     path=".{_format}/{sort?id}",
+     *     defaults={ "_format": "json", "sort": "id" },
+     *     requirements={
+     *         "sort": "id|email",
+     *         "_format": "json|xml"
+     *     },
+     *     methods={ Request::METHOD_GET },
+     *     name="cget"
+     * )
+     *
+     * @Security(
+     *     expression="is_granted('IS_AUTHENTICATED_FULLY')",
+     *     statusCode=401,
+     *     message="`Unauthorized`: Invalid credentials."
+     * )
+     *
+    public function cgetAction(Request $request): Response
+    {
+        $order = $request->get('sort');
+        $result = $this->entityManager
+            ->getRepository(Result::class)
+            ->findBy([], [ $order => 'ASC' ]);
+        $format = Utils::getFormat($request);
+
+        // No hay result?
+        // @codeCoverageIgnoreStart
+        if (empty($result)) {
+            return $this->errorMessage(Response::HTTP_NOT_FOUND, null, $format);    // 404
+        }
+        // @codeCoverageIgnoreEnd
+
+        // Caching with ETag
+        $etag = md5((string) json_encode($result));
+        if ($etags = $request->getETags()) {
+            if (in_array($etag, $etags) || in_array('*', $etags)) {
+                return new Response(null, Response::HTTP_NOT_MODIFIED); // 304
+            }
+        }
+
+        return Utils::apiResponse(
+            Response::HTTP_OK,
+            [ 'result' => array_map(fn ($u) =>  ['user' => $u], $result) ],
+            $format,
+            [
+                self::HEADER_CACHE_CONTROL => 'must-revalidate',
+                self::HEADER_ETAG => $etag,
+            ]
+        );
+    }
+
+
+/*/
+}
+
